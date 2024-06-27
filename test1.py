dc820f3e3d7dd93a6ae9ff7246118aef002c1984
Akhil <akhilk1910@gmail.com>
2024-06-27 10:33:04 +0530

    python fkw modded

---
+++ test1.py
@@ -0,0 +1,37 @@
+x=10
+print(x)
+print(type(x))
+y="\nhello\
+\tworld"
+type(y)
+print(type(y))
+print(y)
+#describing the code 
+z='''
+sjdnfjkl jlsdnfl 
+sdfsd
+ sdfsdfsd 
+ sdfsd
+ sdfs
+ df
+'''
+import math
+print(math.sqrt(25)//2)
+print(z)
+a=b=c=10
+print(a,b,c)
+a,b,c=10,"hey",12.223
+print(a,b,c)
+x=12,"1abg2",True 
+d,e,f=x
+print(d)
+
+w=int(input("first number"))
+q=int(input("second number"))
+res=w+q
+print("result of addn"+str(res))
+print("result of addn of %d and %d is %d"%(w,q,res))
+print("result is",res)
+print("result of addn of {0} and {1} is {2}".format(w,q,res))
+w=2.0+4.3j
+print(w)
